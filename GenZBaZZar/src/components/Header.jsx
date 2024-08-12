9e9db516f2cc1f9574a1d3044a442bacf2401b0e
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2024-08-12 15:53:08 +0000

    my e commerce

---
+++ GenZBaZZar/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+
+
+function Header() {
+    return (
+      <h1 className="text-orange-500">Header</h1>
+    );
+  }
+
+export default Header;
